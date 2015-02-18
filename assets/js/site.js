1b8072704269d5e0a2e8ae5bf9a1a6576712f712
Jan Alonzo <jmalonzo@taguchimail.com>
2015-02-18 22:21:58 +1100

    Start reworking blog and image section layouts

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,8 +1,3 @@
 (function() {
 
-  // FIXME separate CSS then remove this hack
-  var ivs = document.querySelector(".image-view-single img");
-  if (!ivs) return;
-  ivs.classList.remove("caption__media");
-
 })();
